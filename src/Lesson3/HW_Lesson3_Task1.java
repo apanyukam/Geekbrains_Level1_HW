7707410aed772d489497ca5b6be2cba685460c56
apanyukam <apanyuk@gmail.com>
2020-08-31 00:01:28 +0300

    HW_Lesson3 Task1

--- src/Lesson3/HW_Lesson3_Task1.java
+++ src/Lesson3/HW_Lesson3_Task1.java
@@ -10,37 +10,41 @@ package Lesson3;
 import java.util.Scanner;
 
 public class HW_Lesson3_Task1 {
-
+    private static final int MAX_VALUE = 9;
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int i = 9;
+
         System.out.println("Ваша задача угадать число, используя три попытки");
-        while (true) {
-            playLevel(i);
-            System.out.println("Повторить игру?\nВведите 0 или 1\n1 - ДА, 0 - НЕТ\n");
-            int decision = scanner.nextInt();
-            if (decision == 0) {
-                System.out.println("Игра завершена. Спасибо за активное участие!");
-                break;
+        try {
+            while (true) {
+                playGame(MAX_VALUE);
+                System.out.println("Повторить игру?\nВведите 0 или 1\n1 - ДА, 0 - НЕТ\n");
+                int decision = scanner.nextInt();
+                if (decision == 0) {
+                    System.out.println("Игра завершена. Благодарим за активное участие!");
+                    break;
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    private static void playLevel(int range) {
+    private static void playGame(int range) {
 
         int number = (int) (Math.random() * range);
         int y = 0; //  кол-во попыток
         System.out.println("Угадайте число от 0 до " + range);
+
         while (true) {
             if (y == 3) {
-                System.out.println("Это была последняя попытка! Вы проиграли!");
-                System.out.println("Загаданное случайное число = " + number);
+                System.out.println("Это была последняя попытка. Вы проиграли.");
+                System.out.println("Загаданное число = " + number);
                 break;
             }
             int input_number = scanner.nextInt();
             y++;
-
             if (input_number == number) {
                 System.out.println("Вы угадали.");
                 break;
