5c05b82a5fb8adb601132018291988708c431f40
apanyukam <apanyuk@gmail.com>
2020-09-03 23:25:13 +0300

    HW_Lesson5 Task1

--- src/Lesson5/Employee.java
+++ src/Lesson5/Employee.java
@@ -4,15 +4,87 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Employee {
 
-    public int id;
-    public String firstName;
-    public String middleName;
-    public String lastName;
-    public String position;
-    public String mail;
-    public String telephoneNumber;
+    private int id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String position;
+    private String mail;
+    private String telephoneNumber;
     public int age;
-    public int wages;
+    private int wages;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWages() {
+        return wages;
+    }
+
+    public void setWages(int wages) {
+        this.wages = wages;
+    }
 
     public Employee(int id, String firstName, String middleName, String lastName, String position, String mail,
                     String telephoneNumber, int age, int wages){
