a3148ad4f1b380661a20416ef9fe40256ed35073
apanyukam <apanyuk@gmail.com>
2020-09-04 00:23:12 +0300

    HW_Lesson5 some corrections

--- src/Lesson5/Console.java
+++ src/Lesson5/Console.java
@@ -1,8 +1,10 @@
 package Lesson5;
 
 public class Console {
+    private static int SIZE = 5;
+    private static int CHECK_AGE = 40;
     public static void main(String[] args) {
-        Employee[] employee = new Employee[5];
+        Employee[] employee = new Employee[SIZE];
         employee[0] = new Employee(1, "Сергей", "Сергеевич", "Сергеев", "Инженер",
                 "sergeev@mail.ru", "+7(901) 101-25-25", 30, 30000);
         employee[1] = new Employee(2, "Дмитрий", "Дмитриевич", "Дмитриев", "Технолог",
@@ -15,7 +17,7 @@ public class Console {
                 "smirnov@mail.ru", "+7(901) 101-25-29", 48, 50000);
 
         for(int i = 0; i < employee.length; i ++){
-            if(employee[i].age > 40){
+            if(employee[i].age > CHECK_AGE){
                 employee[i].printConsole();
                 System.out.println("----------------------------");
             }
